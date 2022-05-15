cee4be12b9130000067a4796b0c29b3ea8dd499d
amirtha-coder <amirtha.m.muthirulandi@gmail.com>
2022-05-15 12:20:23 +0100

    added object arraay

--- assets/js/howtoplay.js
+++ assets/js/howtoplay.js
@@ -1,6 +1,19 @@
 // render all speech texts
 // object array
-const speech = [{}, {}, {}];
+const speech = [
+  {
+    text: "Hello, I am constable Sam. I am here to eradicate the crime in Birmingham. Unfortunately, I cannot do this alone.",
+    value: 1,
+  },
+  {
+    text: "Here is where you come in. Please help my team and me solve the crime by distributing the resources. Watch out if you go too slow; your progress will be less than 50%, and the game will be over",
+    value: 2,
+  },
+  {
+    text: "Here is where you come in. Please help my team and me solve the crime by distributing the resources. Watch out if you go too slow; your progress will be less than 50%, and the game will be over.",
+    value: 3,
+  },
+];
 const readFromLocalStorage = (key, defaultValue) => {
   // get from LS using key name
   const dataFromLS = localStorage.getItem(key);
