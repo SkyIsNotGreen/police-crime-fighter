48640a22d9d50f02cf142130d73ae235a2f00328
amirtha-coder <amirtha.m.muthirulandi@gmail.com>
2022-05-17 05:18:02 +0100

    js working

--- assets/js/howtoplay.js
+++ assets/js/howtoplay.js
@@ -1,7 +1,7 @@
 // render all speech texts
 // object array
 
-let visibleSpeech = 0;
+let speechIndex = 0;
 
 const speech = [
   {
@@ -13,7 +13,7 @@ const speech = [
     value: 2,
   },
   {
-    text: "Here is where you come in. Please help my team and me solve the crime by distributing the resources. Watch out if you go too slow; your progress will be less than 50%, and the game will be over.",
+    text: "Make good decisions to earn more money and thereby. Good Luck, I know you will make us proud.",
     value: 3,
   },
 ];
@@ -40,22 +40,99 @@ const writeToLocalStorage = (key, value) => {
 };
 
 const typeWriterEffect = () => {};
+
+//   const speechDiv = ` <div class="notification speech-bubble-1">
+//             <button class="delete"></button>
+//             <p>${speech.text}</p>
+//             </div>`;
+//   const showNext = () => {
+//     $(".column speech-bubble-container").append(speechDiv);
+//   };
+// $(document).ready(function () {
+//   $(".notification speech-bubble-1").each(function (e) {
+//     if (e != 0) $(this).hide();
+//   });
+//   const showNext = () => {
+//     if ($(".notification speech-bubble-1:visible").next().length != 0)
+//       $(".notification speech-bubble-1:visible").next().show().prev().hide();
+//     else {
+//       $(".notification speech-bubble-1:visible").hide();
+//       $(".notification speech-bubble-1:first").show();
+//     }
+//     return false;
+//   };
+
+//   const showPrevious = () => {
+//     if ($(".notification speech-bubble-1:visible").prev().length != 0)
+//       $(".notification speech-bubble-1:visible").prev().show().next().hide();
+//     else {
+//       $(".notification speech-bubble-1:visible").hide();
+//       $(".notification speech-bubble-1:last").show();
+//     }
+//     return false;
+//   };
+
+//   );
+
+//   //   const showNext = () => {
+//   //     let nextDiv = $(this)
+//   //       .parent()
+//   //       .find(".notification speech-bubble-1:visible");
+//   //       console.log(find(".notification speech-bubble-1:visible"))
+//   //     if (nextDiv.length === 0) {
+//   //       nextDiv = $(this).parent().find(".notification speech-bubble-1:first");
+//   //     }
+//   //     $(this).parent().find("notification speech-bubble-1").hide();
+//   //     nextDiv.show();
+//   //   };
+
+//   //   const showPrevious = () => {
+//   //     let prevDiv = $(this)
+//   //       .parent()
+//   //       .find(".notification speech-bubble-1:visible")
+//   //       .prev(".notification speech-bubble-1");
+//   //     if (prevDiv.length === 0) {
+//   //       // wrap around to end
+//   //       prevDiv = $(this).parent().find(".notification speech-bubble-1:last");
+//   //     }
+//   //     $(this).parent().find(".notification speech-bubble-1").hide();
+//   //     prevDiv.show();
+//   //     console.log("previous");
+
+//   //   if(){
+
+//   //   } else () {
+
+//   //   }
+// });
+
 const renderSpeechAlerts = () => {
-  console.log(speechAlerts);
-
-  //   if(){
-
-  //   } else () {
-
-  //   }
-  speech.forEach(renderSpeechAlerts);
+  console.log("hello");
+  const speechText = $("#speech-text");
+  const speechToDisplay = speech[speechIndex].text;
+  speechText.text(speechToDisplay);
+  // $(".notification").hide();x
+  // $(".notification:eq(" + visibleDiv + ")").show();
 };
-const startHowToPlay = () => {
+const showNext = () => {
+  console.log(speechIndex);
+  speechIndex = speechIndex === 2 ? 0 : speechIndex + 1;
+
   renderSpeechAlerts();
 };
 
-$("#prev").click("showPrevious");
-$("#next").click("showNext");
+const showPrevious = () => {
+  speechIndex = speechIndex === 0 ? 2 : speechIndex - 1;
+  renderSpeechAlerts();
+};
+
+$("#next").click(showNext);
+$("#previous").click(showPrevious);
+
+const startHowToPlay = () => {
+  handleNavBarToggle();
+  renderSpeechAlerts();
+};
 $(document).ready(() => {
   startHowToPlay();
 });
