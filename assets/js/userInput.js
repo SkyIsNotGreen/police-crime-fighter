ad2e6c362fd3cc06f166ee54c9083389a30a0a6c
amirtha-coder <amirtha.m.muthirulandi@gmail.com>
2022-05-17 20:31:20 +0100

    added resources object- finished

---
+++ assets/js/userInput.js
@@ -0,0 +1,43 @@
+// create a resources object
+const resources = {
+  officer: 20,
+  dog: 15,
+  car: 10,
+  helicopter: 5,
+};
+const writeToLocalStorage = (key, value) => {
+  // convert value to string
+  const stringifiedValue = JSON.stringify(value);
+
+  // set stringified value to LS for key name
+  localStorage.setItem(key, stringifiedValue);
+};
+const readFromLocalStorage = (key, defaultValue) => {
+  // get from LS using key name
+  const dataFromLS = localStorage.getItem(key);
+
+  // parse data from LS
+  const parsedData = JSON.parse(dataFromLS);
+
+  if (parsedData) {
+    return parsedData;
+  } else {
+    return defaultValue;
+  }
+};
+
+const onSubmit = () => {
+  const value = $("#input").val();
+  writeToLocalStorage("username", value);
+  if (readFromLocalStorage("username")) {
+    $("#enter-username").remove();
+    writeToLocalStorage("resources", resources);
+  } else {
+    $("#enter-username").text("please enter username to continue");
+  }
+};
+// target button
+$("#submit").click(onSubmit);
+$(document).ready(() => {
+  handleNavBarToggle();
+});
