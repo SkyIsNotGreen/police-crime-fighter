5706d3f0c178f15747c044e67994f8fe977566ba
amirtha-coder <amirtha.m.muthirulandi@gmail.com>
2022-05-19 20:06:47 +0100

    ls initiated

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,5 +1,7 @@
 console.log("hello from score");
 
+// create an array that stores all past data
+
 const readFromLocalStorage = (key, defaultValue) => {
   // get from LS using key name
   const dataFromLS = localStorage.getItem(key);
@@ -17,9 +19,28 @@ const writeToLocalStorage = (key, value) => {
   // set stringified value to LS for key name
   localStorage.setItem(key, stringifiedValue);
 };
+const scoresOnLoad = () => {
+  // get game stats from LS
+  writeToLocalStorage("pastUserHistory", {});
+  const gameStat = readFromLocalStorage("gameStats");
+  if (!gameStat) {
+    // render messages target
+  } else {
+    const userLog = [
+      { gameStat },
+      //  add score here
+    ];
+    return writeToLocalStorage("pastUserHistory", userLog);
+  }
+
+  console.log(gameStat);
+  // store value in an array
+
+  // delete from LS resources and games stats
+  //
+};
 
 $(document).ready(() => {
   handleNavBarToggle();
+  scoresOnLoad();
 });
-``;
-``;
